23e76094b374ca08a757c217d2f50715bbebd58e
dishu <PalakMaradiya1594@gmail.com>
2023-11-29 18:13:56 +0530

    l-7

---
+++ h5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main(){
+	
+	char i, j;
+	
+	
+	for(i='a'; i<='e'; i++){
+		for(j='a'; j<='e'; j++){
+			printf("%c", i);				
+		}
+		
+		printf("\n");
+		
+		
+	}
+
+
+}
---
+++ h6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main(){
+	
+	char i, j;
+	char k='a';
+	
+	for(i='a'; i<='e'; i++){
+		for(j='a'; j<='e'; j++){
+			printf("%c", k);	
+			k++;			
+		}
+		
+		printf("\n");
+		
+		
+	}
+
+
+}
